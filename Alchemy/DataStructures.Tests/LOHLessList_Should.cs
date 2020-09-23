079c43720615a7b721662d1dcc17a016465f9253
Denis Tarasov <d.tarasov@scout-corp.com>
2020-09-23 23:24:37 +0300

    [dev][add] Remove operation

--- Alchemy/DataStructures.Tests/LOHLessList_Should.cs
+++ Alchemy/DataStructures.Tests/LOHLessList_Should.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -16,6 +17,23 @@ namespace DataStructures.Tests
             }
 
             Assert.AreEqual(48, lohLessList.Count);
+        }
+
+        [TestMethod]
+        public void Return8_OnRemove()
+        {
+            using var lohLessList = new LOHLessList<int>();
+            foreach (int i in Enumerable.Range(1, 48))
+            {
+                lohLessList.Add(i);
+            }
+
+            Assert.IsTrue(lohLessList.Remove(42));
+
+            //List<int> s;
+            //s.Remove()
+
+            //Assert.AreEqual(48, lohLessList.Count);
         }
     }
 }
--- Alchemy/DataStructures/LOHLessList.cs
+++ Alchemy/DataStructures/LOHLessList.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Buffers;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace DataStructures
 {
@@ -11,21 +13,54 @@ namespace DataStructures
         private Slot[] temp = new Slot[17];
 
         public int Count { get; private set; }
-        
+
         public void Add(T item)
         {
             if (_disposed) throw new ObjectDisposedException(nameof(LOHLessList<T>));
 
             var local = temp[_slotNum];
 
-            if (local.Items == null) local.Items = ArrayPool<T>.Shared.Rent(16 << _slotNum);
+            if (local.Items == null) local.Items = ArrayPool<ValueContainer<T>>.Shared.Rent(16 << _slotNum);
 
-            local.Items[local.Index++] = item;
+            local.Items[local.Index++] = new ValueContainer<T> { Value = item };
             temp[_slotNum] = local;
 
             if (local.Items.Length == local.Index) _slotNum++;
 
             Count++;
+        }
+
+        public bool Remove(T item)
+        {
+            for (var i = 0; i < temp.Length; i++)
+            {
+                Slot slot = temp[i];
+                if (slot.Items == null) continue;
+
+                int index = IndexOf(slot.Items, item);
+                if (index < 0) continue;
+
+                slot.Items[index] = new ValueContainer<T>
+                {
+                    Removed = true,
+                    Value = default
+                };
+                temp[i] = slot;
+
+                return true;
+            }
+
+            return false;
+
+            int IndexOf(ValueContainer<T>[] array, T value)
+            {
+                for (int index = 0; index < array.Length; ++index)
+                {
+                    if (EqualityComparer<T>.Default.Equals(array[index].Value, value))
+                        return index;
+                }
+                return -1;
+            }
         }
 
         public void Dispose()
@@ -34,10 +69,10 @@ namespace DataStructures
 
             for (var i = 0; i < temp.Length; i++)
             {
-                var slot = temp[i];
+                Slot slot = temp[i];
                 if (slot.Items == null) continue;
 
-                ArrayPool<T>.Shared.Return(slot.Items, true);
+                ArrayPool<ValueContainer<T>>.Shared.Return(slot.Items, true);
                 slot.Items = null;
                 temp[i] = slot;
             }
@@ -45,10 +80,18 @@ namespace DataStructures
             temp = null;
         }
 
+
+
+        private struct ValueContainer<TValue>
+        {
+            public bool Removed;
+            public TValue Value;
+        }
+
         private struct Slot
         {
             public int Index;
-            public T[] Items;
+            public ValueContainer<T>[] Items;
         }
     }
 }
